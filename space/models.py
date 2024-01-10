e4b4be5419c44e72aa4e07810544db80abe1f2d3
py3-dev <[200~yash.varshney@careers360.in>
2024-01-11 01:24:00 +0530

    space migrations
    register app

--- space/models.py
+++ space/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Space(models.Model):
+    name = models.CharField(max_length=255)
+    slug  = models.SlugField(unique=True)
+    created_by = models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
