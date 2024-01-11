635957a077709e36508d6700325709e1ebc6a404
py3-dev <[200~yash.varshney@careers360.in>
2024-01-11 21:08:38 +0530

    feat: setup asgi server

--- chango/asgi.py
+++ chango/asgi.py
@@ -1,7 +1,18 @@
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter,URLRouter
+import space.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chango.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http":get_asgi_application(),
+    "websocket":AuthMiddlewareStack(
+        URLRouter(
+            space.routing.websocket_urlpatterns
+        )
+    )
+}
+)
